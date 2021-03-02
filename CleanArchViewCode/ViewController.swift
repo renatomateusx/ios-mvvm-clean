27d959b6ed8839e0ec32f598067e6e712e7f7885
Renato Mateus <renatomateusx@gmail.com>
2021-03-02 14:46:19 -0300

    Initial Commit

---
+++ CleanArchViewCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanArchViewCode
+//
+//  Created by Renato Mateus on 02/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
